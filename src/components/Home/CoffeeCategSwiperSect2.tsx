c0ccafe444ab2ac9f2726022289dee0bd02c52df
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-05-29 23:37:58 +0400

    improve: motion && responsive

--- src/components/Home/CoffeeCategSwiperSect2.tsx
+++ src/components/Home/CoffeeCategSwiperSect2.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 
 import {
   MochaCup,
@@ -24,13 +22,14 @@ import {
   BlackCoffeeSlideImg,
   EspressoNavSlideImg,
   MochaNavSlideImg,
-  Bean1,
-  Bean2,
 } from '../../../public/images';
+
 import Image, { StaticImageData } from 'next/image';
+
+import CoffeeCategSliderSwipSec2 from './CoffeeCategSliderSwipSec2';
 import clsx from 'clsx';
 
-interface ICoffee {
+export interface ICoffee {
   id: number;
   name: string;
 
@@ -106,16 +105,18 @@ export default function CoffeCategSwiperSect2() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <section className='relative w-full h-screen'>
-      {/* Custom Pagination */}
-      <div className='absolute  right-1/2 top-1/2  translate-x-1/2 z-10 flex flex-col gap-2  rounded'>
+    <section className='relative w-full h-screen '>
+      <div className='absolute hidden   right-1/2 top-1/2  translate-x-1/2 z-10 lg:flex flex-col gap-2  rounded'>
         {CoffeeCategories.map((el, index) => (
           <div
             key={index}
             onClick={() => swiperRef.current.slideTo(index)}
-            className={`w-[14px] h-[14px] flex items-center  justify-center bg-white rounded-full`}
+            className={clsx(
+              `w-[34px] h-[34px] flex items-center  justify-center  rounded-full`,
+              index === activeIndex ? 'bg-white' : 'bg-transparent'
+            )}
           >
-            <div className='w-3 h-3 rounded-full cursor-pointer transition-all duration-300 overflow-hidden '>
+            <div className='w-[30px] h-[30px] rounded-full cursor-pointer transition-all duration-300 overflow-hidden '>
               <Image
                 src={el.slideImage}
                 alt='image'
@@ -126,81 +127,12 @@ export default function CoffeCategSwiperSect2() {
         ))}
       </div>
 
-      {/* Swiper */}
-      <Swiper
-        direction='vertical'
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-        className='w-full h-full'
-      >
-        {CoffeeCategories.map((el) => (
-          <SwiperSlide key={el.id}>
-            <div className='w-full h-full   flex'>
-              <div
-                className={clsx(
-                  'flex-1  flex justify-center items-center h-full'
-                )}
-                style={{ background: el.bgColor }}
-              >
-                <div className='   flex gap-10 relative'>
-                  <Image
-                    src={el.leaf}
-                    alt={`${el.name} leaf`}
-                    className='max-w-[130px] w-full  max-h-[400px]  absolute -top-30 -right-20 '
-                  />
-                  <Image
-                    src={Bean1}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -left-20  absolute  '
-                  />
-                  <Image
-                    src={Bean1}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -bottom-30 -right-20  absolute  '
-                  />
-                  <Image
-                    src={Bean1}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -bottom-20 right-20  absolute  '
-                  />
-                  <Image
-                    src={Bean1}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -bottom-50 -right-45  absolute  '
-                  />
-                  <Image
-                    src={Bean2}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -bottom-40 right-0  absolute  '
-                  />
-                  <Image
-                    src={Bean2}
-                    alt={'bean image'}
-                    className='max-w-[45px] w-full h-[45px] -bottom-15 -right-30  absolute  '
-                  />
-                  <Image
-                    src={el.spoonImage}
-                    alt={`spoon of ${el.name} `}
-                    className='max-w-[130px] w-full min-h-[200px] max-h-[400px] -rotate-[35deg] absolute top-10 -left-20 '
-                  />
-                  <Image
-                    src={el.image}
-                    alt={el.name}
-                    className='object-contain max-w-[390px] w-full
-min-[300px]'
-                  />
-                </div>
-              </div>
-              <div className='flex-1  h-full  flex items-center justify-center '>
-                <div className='flex max-w-[60%] flex-col text-start text-[#242120]   gap-10 '>
-                  <h3 className='text-4xl'>{el.name}</h3>
-                  <p className=' text-xs'>{el.description}</p>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <CoffeeCategSliderSwipSec2
+        categories={CoffeeCategories}
+        swiperRef={swiperRef}
+        activeIndex={activeIndex}
+        handleSetActiveIndex={setActiveIndex}
+      />
     </section>
   );
 }
